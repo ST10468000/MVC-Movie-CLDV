9d78776805639b6c416f4323258bc366fa4704de
ST10468000 <ST10468000@vcconnect.edu.za>
2026-02-18 09:54:39 +0200

    views steps

--- MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a48ac5703a92228d7ffcb33076ca4c9eb180da4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb26d91b61cb102c951ad1c922322cd877773dfc")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
