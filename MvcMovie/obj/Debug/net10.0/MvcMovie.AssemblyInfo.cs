c9501fcc0bc1232cbda0400a7e18f9d4847f1672
ST10468000 <ST10468000@vcconnect.edu.za>
2026-02-23 20:17:12 +0200

    Part 9

--- MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56bb1f07c812a237dceafc5ac9338757dccb4a9c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d06ee32bda8babb6c76f7c26737831b3cee4798")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
